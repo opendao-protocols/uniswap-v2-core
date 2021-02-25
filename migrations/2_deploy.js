d126d5d44203f5c2ab949c076adf8af436f7100e
akshaysrivastav <akshaysrivastav2014@gmail.com>
2021-02-25 10:38:26 +0530

    Deployed uniswap core contracts

--- migrations/2_deploy.js
+++ migrations/2_deploy.js
@@ -1,6 +1,6 @@
 const UniswapV2Factory = artifacts.require("UniswapV2Factory");
 
-const FEE_TO_SETTER_ADDRESS = "0x0000000000000000000000000000000000000000"
+const FEE_TO_SETTER_ADDRESS = "0xcBc306d7B7C6E711e0Cf43E3A154bA473a79b382"
 
 module.exports = function (deployer) {
   deployer.deploy(UniswapV2Factory, FEE_TO_SETTER_ADDRESS);
--- truffle-config.js
+++ truffle-config.js
@@ -43,12 +43,12 @@ module.exports = {
     // options below to some value.
     //
     development: {
-     host: "127.0.0.1",     // Localhost (default: none)
-     port: 8545,            // Standard Ethereum port (default: none)
-     network_id: "*",       // Any network (default: none)
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 8545,            // Standard Ethereum port (default: none)
+      network_id: "*",       // Any network (default: none)
     },
     xdai: {
-      provider: function() {
+      provider: function () {
         return new HDWalletProvider(
           ["xDAI Account Private Key"],
           "https://dai.poa.network")
@@ -92,15 +92,15 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
-      // version: "0.5.1",    // Fetch exact version from solc-bin (default: truffle's version)
+      version: "0.5.16",    // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
-      // settings: {          // See the solidity docs for advice about optimization and evmVersion
-      //  optimizer: {
-      //    enabled: false,
-      //    runs: 200
-      //  },
-      //  evmVersion: "byzantium"
-      // }
+      settings: {          // See the solidity docs for advice about optimization and evmVersion
+        optimizer: {
+          enabled: true,
+          runs: 200
+        },
+        // evmVersion: "byzantium"
+      }
     },
   },
 };
